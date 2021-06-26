f876f6112b4130f787105a19a119de27dff94bef
ArtusU <artusrock@hotmail.com>
2021-06-26 10:21:34 +0100

    form.js addLead action reducer

--- frontend/src/actions/a_leads.js
+++ frontend/src/actions/a_leads.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { GET_LEADS, DELETE_LEAD } from './types';
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types';
 
 
 export const getLeads = () => (dispatch) => {
@@ -26,4 +26,15 @@ export const getLeads = () => (dispatch) => {
         });
       })
       .catch((err) => console.log(err));
+  };
+
+  export const addLead = (lead) => (dispatch) => {
+    axios.post('/api/leads/', lead)
+    .then((res) => {
+      dispatch({
+        type: ADD_LEAD,
+        payload: res.data,
+      })
+    })
+    .catch((err) => console.log(err));
   };
