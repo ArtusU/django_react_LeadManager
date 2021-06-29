f2ef5f1bb04debd74590dd7d63796c8d40a3876a
ArtusU <artusrock@hotmail.com>
2021-06-29 09:49:27 +0100

    Login Register components

---
+++ frontend/src/components/accounts/Register.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+import { Link, Redirect  } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+export class Register extends Component {
+    state = {
+        username: '',
+        email: '',
+        password: '',
+        password2: '',
+    }
+
+    static propTypes = {
+        prop: PropTypes
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        console.log('Submitted')
+    }
+
+    onChange = (e) => this.setState({ [e.target.name ]: e.target.value })
+
+    render() {
+        const { username, email, password, password2 } = this.state;
+        return (
+            <div className="col-md-6 m-auto">
+                <div className="card card-body mt-5">
+                <h2 className="text-center">Register</h2>
+                    <form onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="username"
+                            onChange={this.onChange}
+                            value={username}
+                        />
+                        </div>
+                        <div className="form-group">
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            name="email"
+                            onChange={this.onChange}
+                            value={email}
+                        />
+                        </div>
+                        <div className="form-group">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            name="password"
+                            onChange={this.onChange}
+                            value={password}
+                        />
+                        </div>
+                        <div className="form-group">
+                        <label>Confirm Password</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            name="password2"
+                            onChange={this.onChange}
+                            value={password2}
+                        />
+                        </div>
+                        <div className="form-group">
+                        <button type="submit" className="btn btn-primary">
+                            Register
+                        </button>
+                        </div>
+                        <p>
+                        Already have an account? <Link to="/login">Login</Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    
+})
+
+const mapDispatchToProps = {
+    
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register)
